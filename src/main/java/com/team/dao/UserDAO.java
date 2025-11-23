3bc0fc0509d3b991839216c360c9ccfbfff0780c
ka11y <elsword485667@gmail.com>
2025-11-23 13:56:16 +0900

    feat: insertUser 추가 및 mailsender 의존성 추가

--- src/main/java/com/team/dao/UserDAO.java
+++ src/main/java/com/team/dao/UserDAO.java
@@ -1,5 +1,32 @@
 package com.team.dao;
 
+import com.team.common.DBUtil;
+import com.team.controller.model.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserDAO {
+
+    public boolean insertUser(User user) {
+        String sql = "INSERT INTO user (user_id, email, password, dept_id, grade) VALUES (?, ?, ?, ?, ?)";
+
+        try (
+                Connection conn = DBUtil.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, user.getUser_id());
+            pstmt.setString(2, user.getEmail());
+            pstmt.setString(3, user.getPassword());
+            pstmt.setInt(4, user.getDept_id());
+            pstmt.setInt(5, user.getGrade());
+
+            int affectedRows = pstmt.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }
